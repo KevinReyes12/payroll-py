305b5bd51c33699af98aa9e4a65c1dfc7b9ffb55
KevinReyes12 <kevnire12@gmail.com>
2023-09-26 12:02:58 -0500

    pyroll

---
+++ payroll/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+    path('',views.inicio , name = 'inicio'),
+    path('nosotros', views.nosotros, name= 'nosotros'),
+    path('empleados', views.empleados, name='empleados'),
+    path('empleados/crear', views.crear_empleado, name='crear'),
+    path('empleados/editar', views.editar_empleado, name='editar'),
+    path('eliminar/<int:id>', views.eliminar, name= 'eliminar'),
+    path('empleados/editar/<int:id>', views.editar_empleado, name='editar'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ payroll/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import Empleado
+from .forms import EmpleadoForm
+#from django.http import HttpResponse
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+
+def nosotros(request):
+    return render(request,'paginas/nosotros.html')
+
+def empleados(request):
+    empleados = Empleado.objects.all()
+    return render(request,'empleados/index.html',{'empleados':empleados})
+
+def crear_empleado(request):
+    formulario = EmpleadoForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('empleados')
+    return render(request, 'empleados/crear.html', {'formulario':formulario})
+
+def editar_empleado(request,id):
+    empleado = Empleado.objects.get(id=id)
+    formulario = EmpleadoForm(request.POST or None, request.FILES or None, instance=empleado) 
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('empleados')
+    return render(request, 'empleados/editar.html', {'formulario':formulario})
+
+def eliminar(request, id):
+    empleado = Empleado.objects.get(id=id)
+    empleado.delete()
+    return redirect('empleados')
