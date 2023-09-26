c457b74416c3a0f7bbf9c02c184db1790012c7f1
KevinReyes12 <kevnire12@gmail.com>
2023-09-26 00:06:35 -0500

    Django

---
+++ payroll/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Empleado(models.Model):
+    ci = models.IntegerField(unique=True, verbose_name= "C.I.")
+    name = models.CharField(max_length=75, verbose_name="Apellidos y Nombres")
+    position = models.CharField(max_length=50, verbose_name="Cargo")
+    pay = models.IntegerField(verbose_name="Sueldo")
+    comissions = models.IntegerField(verbose_name="Comisiones")
+
+    def __str__(self):
+        fila = "C.I.: "+ str(self.ci) + " Nombre: "+ self.name
+        return fila
