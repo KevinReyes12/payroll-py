78b84e52f197f78a716d6ab552a0082e6b565bcb
KevinReyes12 <kevnire12@gmail.com>
2023-10-15 15:31:24 -0500

    UpdateLogin

--- roles/settings.py
+++ roles/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'roles.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -146,3 +146,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
