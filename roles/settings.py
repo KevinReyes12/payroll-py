38c9283b82d834d0c79d1989db380f23475103c7
KevinReyes12 <kevnire12@gmail.com>
2023-10-15 00:15:30 -0500

    Update

--- roles/settings.py
+++ roles/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 DATABASE_URL = env('DB_URL')
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c8fd.up.railway.app']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
