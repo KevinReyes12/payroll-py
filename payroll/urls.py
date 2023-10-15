377d983a62575430792a5bf47690ce1f842965f5
KevinReyes12 <kevnire12@gmail.com>
2023-10-15 16:14:22 -0500

    UpdateLogin2

--- payroll/urls.py
+++ payroll/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
-    path('',views.inicio , name = 'inicio'),
+    path('inicio',views.inicio , name = 'inicio'),
     path('nosotros', views.nosotros, name= 'nosotros'),
     path('empleados', views.empleados, name='empleados'),
     path('empleados/crear', views.crear_empleado, name='crear'),
